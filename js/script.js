81c695672bf029c0ebdbf64ea5f6659467f3695c
JammiM <jammim19@gmail.com>
2016-11-14 23:58:43 +0000

    Fixing a bug with column ordering

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-  
-  alert("Hello! I am an alert box!!");
+
+
+
+/*
+  $('#courses h3').animate({
+    opacity: 0.25,
+    left: "+=50",
+    height: "toggle"
+  }, 5000, function() {
+    // Animation complete.
+  });
+
+*/
+
 
 });
